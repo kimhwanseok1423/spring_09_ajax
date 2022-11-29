af9da755ff1ec69ce852b90e02702a9c1cf5542d
kimhwanseok <kcsss4499@gmail.com>
2022-11-29 17:28:55 +0900

    update

--- spring_13_scheduled/src/main/java/scheduler/SchedulerBean.java
+++ spring_13_scheduled/src/main/java/scheduler/SchedulerBean.java
@@ -6,7 +6,27 @@ import java.util.Calendar;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
-
+/*  로그(log) 프로그램 개발이나 웅영시 문제점을 추적하거나 운영상태를 모니터링하기 위한 기능이다.
+ *  System.out.println();을 사용하여 로그를 확인 할 수 있으나 이보다는 로그를 기록하는 방법을 사용한다.
+ *  log4j은 다음과 같은 레벨을 가진다.
+ *  TRACE > DEBUG >INFO >WARN >ERROR > FATAL
+ *  만약에 info로 세팅하면 INFO,WARN,ERROR,FATAL은 기록된다.
+ *  
+ *   FATAL: 심각한 에러-> 프로그램 자체가 실행되지 않을 수있다.
+ *   ERROR: 문제가 발생하면 소프트웨어 일부가 기능을 수행하지 못할수있다.
+ *   WARNING: 소프트웨어가 동작하고 있지만, 예상지 못한 일이 발생하고 가까운 미래에 문제가 발생할 수 있다는 경고
+ *               예로 디스크 공간 부족->프로ㅗ그램은 동작되지만 앞으로 문제가 발생할수 있음.
+ *   
+ *   INFO:작업이 정상적으로 작동하고 있음을 메시지를 통해 확인하고  싶은 경우에 사요ㅗㅇ
+ *   DEBUG:어떤 문제가 발생했을 경우 문제를 진단하고 싶을때 필요한 자세한 정보를 기록한다.
+ *   개발시 디버그 용도로 사용하는 메시지를 나타낸다.
+ *   
+ *   TRACE:	 디버그 레벨이 너무 광범위한 것을 해결하기 위해서 좀더 상세한 이벤트를 나타낸다.
+ *   
+ * 
+ * 
+ * 
+ */
 public class SchedulerBean {
 private static final Logger logger=LoggerFactory.getLogger(SchedulerBean.class);
 
